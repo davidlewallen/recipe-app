293e0e31091e36c15630c7420a8e7efb4eaf2eb9
davidlewallen <lewallen.david@gmail.com>
2017-12-27 21:41:12 -0600

    Updated register function to include new schema

--- server/routes/account.js
+++ server/routes/account.js
@@ -8,16 +8,23 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json(req.user);
 });
 
-router.post('/register', function (req, res) {
+router.post('/register', (req, res) => {
   Account.register(
-    new Account({ username: req.body.username }),
+    new Account({
+      username: req.body.username,
+      info: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+      },
+    }),
     req.body.password,
-    function (err, account) {
+    (err) => {
       if (err) {
         console.log('error', err);
-        return res.send(err, 409);
+        return res.status(409).send(err);
       }
-  passport.authenticate('local')(req, res, function () {
+  passport.authenticate('local')(req, res, () => {
       res.send(req.user)
     });
   });
