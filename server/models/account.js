968c81ebf8ca4b67896a71561891de3a7e5738fe
davidlewallen <lewallen.david@gmail.com>
2017-12-27 21:40:55 -0600

    Updated Account schema to have an info field for personal info

--- server/models/account.js
+++ server/models/account.js
@@ -3,8 +3,31 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const Account = new Schema({
-  username: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+  },
+  password: { type: String },
+  info: {
+    firstName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+  },
   savedRecipes: [Schema.Types.ObjectId],
 });
 
