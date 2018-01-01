9b5faea477027e901de711f1e0787a635f7780db
davidlewallen <lewallen.david@gmail.com>
2018-01-01 14:52:34 -0600

    decoupled express from controllers for testing purposes

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -7,10 +7,35 @@ const Recipe = require('../controllers/recipe');
 
 router.use(isAuthenticated);
 
-router.get('/', Recipe.get);
+router.get('/', async (req, res) => {
+  try {
+    res.json(await Recipe.get(req.user._id))
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
-router.post('/submit/:recipeURL',  Recipe.submit);
+router.post('/submit/:recipeURL',  async (req, res) => {
+  try {
+    const result = await Recipe.submit(req.params.recipeURL, req.user._id);
+    
+    if (result.nonProcessableWebsite) {
+      res.status(403).send({ result });
+    } else {
+      res.json(result);
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
-router.delete('/delete/:recipeId', Recipe.remove);
+// router.delete('/delete/:recipeId', Recipe.remove);
+router.delete('/delete/:recipeId', async (req, res) => {
+  try {
+    res.json(await Recipe.remove(req.params.recipeId, req.user._id));
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 module.exports = router;
