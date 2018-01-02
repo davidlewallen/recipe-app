bef9d58e09ab2a8bcf57d781bf67843082d39f19
davidlewallen <lewallen.david@gmail.com>
2018-01-01 19:04:19 -0600

    added spec for models

---
+++ server/models/spec/account.spec.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+
+// Set up mongo connection
+require('../../db');
+
+const Account = require('../account');
+
+const registerAccount = () => new Promise((resolve, reject) => Account.register(
+  new Account({
+    username: 'test',
+    info: {
+      firstName: 'test',
+      lastName: 'test',
+      email: 'test',
+    },
+  }),
+  new Buffer('test'),
+  (err) => {
+    if (err) { reject(); }
+    console.log(2);
+    resolve(true);
+  }
+));
+
+describe('Account Model Test', () => {
+  beforeAll(async () => {
+    await Account.remove({});
+  })
+
+  beforeEach(async () => {
+    await Account.remove({});
+  });
+
+  afterAll(() => {
+    mongoose.connections.close();
+  });
+
+  describe('save', () => {
+    it('should save an account to the account collection', async () => {
+      let result = await Account.find({});
+      expect(result.length).toEqual(0);
+
+      const newAccount = new Account({
+        username: 'testUsername',
+        password: 'testPassword',
+        info: {
+          firstName: 'testFirstName',
+          lastName: 'testLastName',
+          email: 'test@email.com',
+        }
+      });
+      await newAccount.save();
+
+      result = await Account.find({});
+      expect(result.length).toEqual(1);
+    });
+
+    it('should return an error if correct fields arent passed when saving an account', async () => {
+      const newAccount = new Account({
+        password: 'testPassword',
+        info: {
+          firstName: 'testFirstName',
+          lastName: 'testLastName',
+          email: 'test@email.com',
+        }
+      });
+
+      try {
+        await newAccount.save();
+      } catch (err) {
+        expect(err.name).toEqual('ValidationError')
+        expect(err._message).toEqual('Account validation failed')
+      }
+    })
+  });
+
+  describe.only('register', () => {
+    it('should register a new account in account collection', async () => {
+        let result = await Account.find({});
+        console.log('result', result)
+        expect(result.length).toEqual(0);
+        console.log(1);
+        await registerAccount();
+        console.log(3);
+
+        result = await Account.find({});
+        console.log('result', result)
+    });
+  });
+})
