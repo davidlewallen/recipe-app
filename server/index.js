5cd4cc931254990dd35f5b4a8c020ff106879da9
davidlewallen <lewallen.david@gmail.com>
2018-01-08 18:54:22 -0600

    only require dotenv when in test/dev

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'prod') require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
