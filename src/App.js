3eac8cddfa2b75d75be0b4b334116d6dc2cbb475
davidlewallen <lewallen.david@gmail.com>
2018-01-08 18:35:49 -0600

    removed logging

--- src/App.js
+++ src/App.js
@@ -48,7 +48,6 @@ class App extends Component {
   }
 
   submitRecipe = async (event) => {
-    console.log('submit');
     event.preventDefault();
 
     try {
@@ -56,8 +55,7 @@ class App extends Component {
       const { data } = await axios.post(`/api/recipe/submit/${encodedRecipeURI}`)
 
       if (data.alreadyAdded === false) {
-        console.log('not added');
-        this.setState({ recipeList: [...this.state.recipeList, data] }, () => console.log('this.state.recipeList', this.state.recipeList))
+        this.setState({ recipeList: [...this.state.recipeList, data] })
       }
 
     } catch (err) {
