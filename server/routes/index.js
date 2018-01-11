963ac546d0cab144940a5a60b254015d1460ecdb
davidlewallen <lewallen.david@gmail.com>
2018-01-11 12:20:53 -0600

    Added 404 on no-match

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,10 @@ router.use('/account', require('./account'));
 
 router.get('/auth', isAuthenticated, (req, res) => {
   res.send(true);
-})
+});
+
+router.get('*', (req, res) => {
+  res.status(404).send('Endpoint Not Found');
+});
 
 module.exports = router;
