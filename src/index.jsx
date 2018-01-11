7dfdb92d2a5a92c4cc4146d9f831631b28238f56
davidlewallen <lewallen.david@gmail.com>
2018-01-10 19:43:10 -0600

    added router and renamed to .jsx

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+
+import HomepageContainer from './common/homepage/container/HomepageContainer';
+import Account from './account/routes';
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={HomepageContainer} />
+      <Route path="/account" component={Account} />
+    </Switch>
+  </Router>
+  , document.getElementById('root'),
+);
+
+registerServiceWorker();
