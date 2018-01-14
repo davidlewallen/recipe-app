f92060bdec3aa558ed750302d0ba7f904430fde8
davidlewallen <lewallen.david@gmail.com>
2018-01-13 23:34:13 -0600

    implemented better error handling of login endpoint

--- server/routes/account.js
+++ server/routes/account.js
@@ -4,13 +4,54 @@ const passport = require('passport');
 
 const Account = require('../models/account');
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
-  const userObject = {
-    _id: req.user._id,
-    username: req.user.username,
-    savedRecipes: req.user.savedRecipes,
+router.post('/login', (req, res, next) => {
+  if (req.body.username === undefined || req.body.password === undefined) {
+    return res
+      .status(400)
+      .json({
+        message: 'Body should contain a "username" and "password" field'
+      });
+  } else if (!req.body.username) {
+    return res
+      .status(400)
+      .json({
+        message: 'Enter a valid username'
+      });
+  } else if (!req.body.password) {
+    return res
+    .status(400)
+    .json({
+      message: 'Enter a valid password'
+    });
   }
-  res.json(userObject);
+
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.log('err', err);
+      return next(err);
+    }
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: 'Incorrect username and/or password.'});
+    }
+
+    req.login(user, loginErr => {
+      if (loginErr) {
+        console.log('loginErr', loginErr);
+        return next(loginErr);
+      }
+
+      const userObject = {
+        _id: req.user.id,
+        username: req.user.username,
+        savedRecipes: req.user.savedRecipes,
+      };
+
+      return res.json(userObject);
+    });
+  })(req, res, next);
 });
 
 router.post('/register', (req, res) => {
