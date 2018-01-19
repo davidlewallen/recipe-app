74c1554d4bfe4e9fe368274cc9035ab1aa9d2dc2
davidlewallen <lewallen.david@gmail.com>
2018-01-18 19:15:47 -0600

    updated to totalTime

--- src/dashboard/scenes/dashboard/components/spec/index.spec.jsx
+++ src/dashboard/scenes/dashboard/components/spec/index.spec.jsx
@@ -9,12 +9,24 @@ describe('Dashboard snapshot test', () => {
     handleRecipe: () => {},
     submitRecipe: () => {},
     deleteRecipe: () => {},
-    recipeList: [{ title: 'Recipe 1', _id: 1 }, { title: 'Recipe 2', _id: 2 }],
+    recipeList: [{ title: 'Recipe 1', _id: 1, totalTime: '1 hour 10 minutes' }, { title: 'Recipe 2', _id: 2 }],
   };
 
   it('should match snapshot', () => {
     const component = shallow(<Dashboard {...props} />);
 
     expect(component).toMatchSnapshot();
+  });
+
+  it('should display "n/a" for time if totalTime is undefined', () => {
+    const component = shallow(<Dashboard {...props} recipeList={[{ title: 'Recipe', _id: 1 }]} />);
+    const recipe = component.find('.recipe');
+    const footer = recipe.find('.footer');
+    const time = footer.find('Col');
+    // console.log('recipe.debug()', recipe.debug())
+    // console.log('footer.debug()', footer.debug())
+    console.log(time.length)
+    console.log('time.debug()', time.debug())
+    expect(time.at(1).text()).toBe('n/a');
   });
 });
