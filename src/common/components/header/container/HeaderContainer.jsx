625e0fe7e14116f37f40fa71c37bb5f918f39949
davidlewallen <lewallen.david@gmail.com>
2018-01-22 00:26:49 -0600

    Added routes and state

--- src/common/components/header/container/HeaderContainer.jsx
+++ src/common/components/header/container/HeaderContainer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router-dom';
 
 import { Account } from '../../../utils/api';
 
 import Header from '../components';
+import SubmitRecipe from '../../submit-recipe/container/SubmitRecipeContainer';
+import HomepageContainer from '../../../scenes/homepage/container/HomepageContainer';
+import DashboardRoutes from '../../../../dashboard/routes';
+import AccountRoutes from '../../../../account/routes';
 
 const { shape, func } = PropTypes;
 const propTypes = {
@@ -11,13 +16,56 @@ const propTypes = {
 };
 
 class HeaderContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showModal: false,
+      recipes: [],
+    };
+  }
+
   logout = async () => {
     await Account.logout();
     this.props.history.replace('/');
   }
 
+  handleModalOpen = () => {
+    this.setState({ showModal: true });
+  }
+
+  handleModalClose = () => {
+    this.setState({ showModal: false });
+  }
+  updateRecipes = (updatedRecipes) => {
+    this.setState(prevState => ({ recipes: [...prevState.recipes, ...updatedRecipes] }));
+  }
+
   render = () => (
-    <Header logout={this.logout} />
+    <div>
+      <SubmitRecipe
+        show={this.state.showModal}
+        handleModalClose={this.handleModalClose}
+        updateRecipes={this.updateRecipes}
+      />
+      <Header
+        logout={this.logout}
+        handleModalOpen={this.handleModalOpen}
+      />
+      <Switch>
+        <Route exact path="/" component={HomepageContainer} />
+        <Route
+          path="/dashboard"
+          render={() => (
+            <DashboardRoutes
+              recipes={this.state.recipes}
+              updateRecipes={this.updateRecipes}
+            />
+          )}
+        />
+        <Route path="/account" component={AccountRoutes} />
+      </Switch>
+    </div>
   );
 }
 
