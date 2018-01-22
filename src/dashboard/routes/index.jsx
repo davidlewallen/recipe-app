cb38f8b5758795ffef32eb0e94389fe9cea56381
davidlewallen <lewallen.david@gmail.com>
2018-01-22 00:27:22 -0600

    passing global state and updates down

--- src/dashboard/routes/index.jsx
+++ src/dashboard/routes/index.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 
 import DashboardContainer from '../scenes/dashboard/container/DashboardContainer';
 
-const DashboardRoutes = () => (
+const { arrayOf, object, func } = PropTypes;
+const propTypes = {
+  recipes: arrayOf(object.isRequired).isRequired,
+  updateRecipes: func.isRequired,
+};
+
+const DashboardRoutes = props => (
   <Switch>
-    <Route path="/dashboard" component={DashboardContainer} />
+    <Route
+      path="/dashboard"
+      render={() => (
+        <DashboardContainer
+          recipes={props.recipes}
+          updateRecipes={props.updateRecipes}
+        />
+      )}
+    />
   </Switch>
 );
 
+
+DashboardRoutes.propTypes = propTypes;
 export default DashboardRoutes;
