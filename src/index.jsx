d419b5727ed91d707ddbbbd705615948cd8f6e41
davidlewallen <lewallen.david@gmail.com>
2018-01-23 22:42:46 -0600

    removed commented lines

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import registerServiceWorker from './registerServiceWorker';
 
-// import HomepageContainer from './common/scenes/homepage/container/HomepageContainer';
-// import DashboardRoutes from './dashboard/routes';
-// import Account from './account/routes';
 import HeaderContainer from './common/components/header/container/HeaderContainer';
 
 import './common/assets/styles/app.css';
