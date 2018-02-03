5c33ab7a7936996ddaea4e2e24a78492e7f87039
davidlewallen <lewallen.david@gmail.com>
2018-02-02 23:17:49 -0600

    updated style

--- src/common/scenes/homepage/components/index.jsx
+++ src/common/scenes/homepage/components/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Homepage = () => (
@@ -14,11 +14,10 @@ const Homepage = () => (
         className="login align-center"
         xs={12}
       >
-        <Link
-          className="link align-center border"
-          to="/account/login"
-        >
-          Login
+        <Link to="/account/login">
+          <Button block bsStyle="primary">
+            Login
+          </Button>
         </Link>
       </Col>
     </Row>
