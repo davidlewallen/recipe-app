eb8fde0c43affecb91df01410c962e7ebd641edf
davidlewallen <lewallen.david@gmail.com>
2018-02-03 11:22:44 -0600

    Updated header index to account for conditionals based on isAuth

--- src/common/components/header/spec/index.spec.jsx
+++ src/common/components/header/spec/index.spec.jsx
@@ -30,12 +30,36 @@ describe('Header component', () => {
     expect(mockProps.logout).toHaveBeenCalledTimes(1);
   });
 
-  it('logo should link to "/dashboard" if isAuth = true', () => {
+  it('logo should link to "/dashboard" if isAuth is true', () => {
     expect(wrapper.find('Link').prop('to')).toBe('/dashboard');
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it('logo should link to "/" if isAuth = false', () => {
+  it('logo should link to "/" if isAuth is false', () => {
     wrapper.setProps({ isAuth: false });
     expect(wrapper.find('Link').prop('to')).toBe('/');
-  })
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should not display menu if isAuth is false', () => {
+    wrapper.setProps({ isAuth: false });
+    expect(wrapper.find('NavbarToggle').exists()).toBe(false);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should display menu if isAuth is true', () => {
+    expect(wrapper.find('NavbarToggle').exists()).toBe(true);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should not display + Add Recipe and Logout if isAuth is false', () => {
+    wrapper.setProps({ isAuth: false });
+    expect(wrapper.find('NavbarCollapse').exists()).toBe(false);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should display + Add Recipe and Logout if isAuth is true', () => {
+    expect(wrapper.find('NavbarCollapse').exists()).toBe(true);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
