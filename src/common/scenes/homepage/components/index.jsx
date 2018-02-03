2b43c96e5b90c87b6f92a98583eb59a19158bdf1
davidlewallen <lewallen.david@gmail.com>
2018-02-02 22:31:14 -0600

    updated css to be in styles

--- src/common/scenes/homepage/components/index.jsx
+++ src/common/scenes/homepage/components/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-import '../assets/index.css';
-
 const Homepage = () => (
   <Grid className="homepage align-center">
     <Row>
--- src/common/scenes/homepage/container/HomepageContainer.jsx
+++ src/common/scenes/homepage/container/HomepageContainer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Homepage from '../components';
 
+import '../assets/styles/index.css';
+
 class HomepageContainer extends React.Component {
   componentWillMount() {
     console.log('homepage');
