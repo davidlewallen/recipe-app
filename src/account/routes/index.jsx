bafb36a30b17b2f36fd4a25014f2bb65d6eee8ce
davidlewallen <lewallen.david@gmail.com>
2018-02-03 14:04:31 -0600

    Added overview container to route

--- src/account/routes/index.jsx
+++ src/account/routes/index.jsx
@@ -2,17 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import Overview from '../scenes/overview/components';
 import LoginContainer from '../scenes/login/container/LoginContainer';
 import RegisterContainer from '../scenes/register/container/RegisterContainer';
 
-const { bool, func } = PropTypes;
+const {
+  bool,
+  func,
+  objectOf,
+  string,
+} = PropTypes;
 const propTypes = {
   isAuth: bool.isRequired,
   updateAuth: func.isRequired,
+  user: objectOf(string.isRequired).isRequired,
+  updateUser: func.isRequired,
 };
 
 const AccountRoutes = props => (
   <Switch>
+    <Route
+      exact
+      path="/account"
+      render={() => (
+        props.isAuth
+          ? <Overview user={props.user} updateUser={props.updateUser} />
+          : <Redirect to="/account/login" />
+      )}
+    />
     <Route
       path="/account/login"
       render={routeProps => (
