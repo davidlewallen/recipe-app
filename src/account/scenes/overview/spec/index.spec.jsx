499a8f5492743164815fceeb7d7d7ee2d8b85b0a
davidlewallen <lewallen.david@gmail.com>
2018-02-03 17:34:14 -0600

    Added test for overview component

---
+++ src/account/scenes/overview/spec/index.spec.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Overview from '../components';
+
+describe('Overview', () => {
+  it('should render and match snapshot', () => {
+    const wrapper = shallow(<Overview user={{ username: 'testName', email: 'test' }} />);
+    expect(wrapper.find('Grid').exists()).toBe(true);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
