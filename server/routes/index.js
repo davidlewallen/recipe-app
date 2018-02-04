045617f9beb54f0e1cbb5978237e6a3aeae717c3
davidlewallen <lewallen.david@gmail.com>
2018-02-03 19:38:41 -0600

    added endpoint to return a list of accepted websites

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const listOfApprovedWebsites = require('./utils/listOfApprovedWebsites.js');
 
 router.use('/recipe', require('./recipe'));
 router.use('/account', require('./account'));
 
+router.get('/approved', (req, res) => {
+  res.json(listOfApprovedWebsites());
+});
+
 router.get('*', (req, res) => {
   res.status(404).send('Endpoint Not Found');
 });
+
 
 module.exports = router;
