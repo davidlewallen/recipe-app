3997f9e077fcb7a2c0c703b1da9c427325135506
davidlewallen <lewallen.david@gmail.com>
2018-02-04 22:28:30 -0600

    typed randomKeys

--- src/common/utils/randomKeys.js
+++ src/common/utils/randomKeys.js
@@ -1,7 +1,8 @@
+// @flow
 import uuid from 'uuid/v5';
 
 const NAMESPACE = 'f829e458-5757-54ac-b4b7-598f44c2c9b5';
 
-const genKey = value => uuid(value.toString(), NAMESPACE);
+const genKey = (value: String | Number): String => uuid(value.toString(), NAMESPACE);
 
 export default genKey;
