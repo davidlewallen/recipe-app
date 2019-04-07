921343d6a5d81b9a8673c7b6252da544ffb01349
davidlewallen <lewallen.david@gmail.com>
2019-04-07 13:52:17 -0500

    330 - Added { withCredentials: true } for cross-origin cookies for subdomain (www)

--- src/common/utils/api.js
+++ src/common/utils/api.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 const API_ROOT =
   process.env.NODE_ENV === 'production'
-    ? 'https://api.mysavedrecipes.com/api'
+    ? 'http://mysavedrecipes.com/api'
     : '/api';
 const API_RECIPE = `${API_ROOT}/recipe`;
 const API_ACCOUNT = `${API_ROOT}/account`;
 
 export const Recipe = {
-  getRecipes: () => axios.get(`${API_RECIPE}`),
-  submitRecipe: encodedURI => axios.post(`${API_RECIPE}/submit/${encodedURI}`),
-  deleteRecipe: recipeId => axios.delete(`${API_RECIPE}/delete/${recipeId}`),
+  getRecipes: () => axios.get(`${API_RECIPE}`, { withCredentials: true }),
+  submitRecipe: encodedURI => axios.post(`${API_RECIPE}/submit/${encodedURI}`, { withCredentials: true }),
+  deleteRecipe: recipeId => axios.delete(`${API_RECIPE}/delete/${recipeId}`, { withCredentials: true }),
 };
 
 export const Account = {
@@ -31,25 +31,25 @@ export const Account = {
     return axios.post(this.endpoints.login, body, { withCredentials: true });
   },
   register(body) {
-    return axios.post(this.endpoints.register, body);
+    return axios.post(this.endpoints.register, body, { withCredentials: true });
   },
   logout() {
-    return axios.get(this.endpoints.logout);
+    return axios.get(this.endpoints.logout, { withCredentials: true });
   },
   auth() {
-    return axios.get(this.endpoints.auth);
+    return axios.get(this.endpoints.auth, { withCredentials: true });
   },
   getUser() {
     return axios.get(this.endpoints.getUser, { withCredentials: true });
   },
   verify(id, key) {
-    return axios.get(this.endpoints.verify(id, key));
+    return axios.get(this.endpoints.verify(id, key), { withCredentials: true });
   },
   resendVerification(id) {
-    return axios.get(this.endpoints.resendVerification(id));
+    return axios.get(this.endpoints.resendVerification(id), { withCredentials: true });
   },
 };
 
 export const Utils = {
-  getAcceptedWebsites: () => axios.get(`${API_ROOT}/approved`),
+  getAcceptedWebsites: () => axios.get(`${API_ROOT}/approved`, { withCredentials: true }),
 };
