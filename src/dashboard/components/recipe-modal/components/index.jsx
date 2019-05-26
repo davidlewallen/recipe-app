3d9550921b5aef44e123c6bc64654ccc2ec52363
davidlewallen <lewallen.david@gmail.com>
2019-05-26 15:15:44 -0500

    302 Initial refactor of submit recipe container and added ability to manually submit recipes

--- src/dashboard/components/recipe-modal/components/index.jsx
+++ src/dashboard/components/recipe-modal/components/index.jsx
@@ -25,51 +25,58 @@ const RecipeModal = ({
   handleModalClose,
   selectedRecipe,
   deleteRecipe,
-}) => (
-  <Modal show={showModal} onHide={handleModalClose} className="recipe-modal">
-    <Modal.Header closeButton className="header">
-      <Modal.Title className="title">
-        <span>{selectedRecipe.title}</span>
-        <span className="total-time">
-          {`Total Time: ${selectedRecipe.totalTime || 'n/a'}`}
-        </span>
-      </Modal.Title>
-    </Modal.Header>
-    <Modal.Body className="body">
-      <div className="ingredient-title">Ingredients:</div>
-      {selectedRecipe.ingredients.map(ingredient => (
-        <div className="ingredient" key={genKey(ingredient)}>
-          {`- ${ingredient}`}
-        </div>
-      ))}
+}) => {
+  function handleDeleteRecipe() {
+    deleteRecipe(selectedRecipe._id);
+    handleModalClose();
+  }
 
-      <div className="instruction-title">Instructions:</div>
-      {selectedRecipe.instructions.map(instruction => (
-        <div className="instruction" key={genKey(instruction)}>
-          {instruction}
-        </div>
-      ))}
-    </Modal.Body>
-    <Modal.Footer>
-      <a
-        className="recipe-link"
-        target="_blank"
-        rel="noopener noreferrer"
-        href={selectedRecipe.url.href}
-      >
-        Click here to view original recipe
-      </a>
-      <Button
-        bsStyle="danger"
-        bsSize="small"
-        onClick={() => deleteRecipe(selectedRecipe._id)}
-        type="button"
-      >
-        Delete
-      </Button>
-    </Modal.Footer>
-  </Modal>
-);
+  return (
+    <Modal show={showModal} onHide={handleModalClose} className="recipe-modal">
+      <Modal.Header closeButton className="header">
+        <Modal.Title className="title">
+          <span>{selectedRecipe.title}</span>
+          <span className="total-time">
+            {`Total Time: ${selectedRecipe.totalTime || 'n/a'}`}
+          </span>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="body">
+        <div className="ingredient-title">Ingredients:</div>
+        {selectedRecipe.ingredients.map(ingredient => (
+          <div className="ingredient" key={genKey(ingredient)}>
+            {`- ${ingredient}`}
+          </div>
+        ))}
+
+        <div className="instruction-title">Instructions:</div>
+        {selectedRecipe.instructions.map(instruction => (
+          <div className="instruction" key={genKey(instruction)}>
+            {instruction}
+          </div>
+        ))}
+      </Modal.Body>
+      <Modal.Footer>
+        <a
+          className="recipe-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={selectedRecipe.url.href}
+        >
+          Click here to view original recipe
+        </a>
+        <Button
+          bsStyle="danger"
+          bsSize="small"
+          onClick={handleDeleteRecipe}
+          type="button"
+        >
+          Delete
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
 RecipeModal.propTypes = propTypes;
 export default RecipeModal;
