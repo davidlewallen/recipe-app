160b450295c1c5be035da79c6bb9b099bf933597
David Lewallen <dlewallen@nav.com>
2020-03-01 00:48:54 -0700

    feat(utils): adds get utility

---
+++ src/common/utils/get.js
@@ -0,0 +1,11 @@
+export default (object, path, value) => {
+  if (!path) return undefined;
+
+  const pathArray = Array.isArray(path)
+    ? path
+    : path.split(/[,[\].]/g).filter(Boolean);
+
+  return (
+    pathArray.reduce((prevObj, key) => prevObj && prevObj[key], object) || value
+  );
+};
